4fbedd26b547c8d07327b0beeff6087433a998a2
Nils <nilsstnurfuersam@gmail.com>
2017-01-24 08:49:29 +0100

    decorators do not use the same Object anymore

--- src/aufgabe4/Runner.java
+++ src/aufgabe4/Runner.java
@@ -12,34 +12,32 @@ public class Runner {
     private static final String marker =  "******";
 
     public static void main(String[] args) {
-        AusgabeAggregator ausgA = new AusgabeAggregator();
-        Konsolenausgabe konsA = new Konsolenausgabe();
-
+        
         System.out.println(System.lineSeparator() +  marker + "LetterCounter" + marker);
-        LetterCounter lcA = new LetterCounter(ausgA);
-        LetterCounter lcK = new LetterCounter(konsA);
+        LetterCounter lcA = new LetterCounter(new AusgabeAggregator());
+        LetterCounter lcK = new LetterCounter(new Konsolenausgabe());
         lcA.print("Hallo");
         lcK.print("Hallo");
         System.out.println(lcA.getCount());
         System.out.println(lcK.getCount());
 
         System.out.println(System.lineSeparator() + marker + "UpperCase" + marker);
-        UpperCase ucA = new UpperCase(ausgA);
-        UpperCase ucK = new UpperCase(konsA);
+        UpperCase ucA = new UpperCase(new AusgabeAggregator());
+        UpperCase ucK = new UpperCase(new Konsolenausgabe());
         ucA.print("Hallo");
         ucK.print("Hallo");
 
         System.out.println(System.lineSeparator() + marker + "WordCounter" + marker);
-        WordCounter wcA = new WordCounter("Hallo", ausgA);
-        WordCounter wcK = new WordCounter("Hallo", konsA);
+        WordCounter wcA = new WordCounter("Hallo", new AusgabeAggregator());
+        WordCounter wcK = new WordCounter("Hallo", new Konsolenausgabe());
         wcA.print("Hallo Welt Hallo hallo");
         wcK.print("Hallo Welt Hallo hallo");
         System.out.println(wcA.getWordCount());
         System.out.println(wcK.getWordCount());
 
         System.out.println(System.lineSeparator() + marker + "PostfixAdder" + marker);
-        PostfixAdder cfA = new PostfixAdder("chen", ausgA);
-        PostfixAdder cfK = new PostfixAdder("chen", konsA);
+        PostfixAdder cfA = new PostfixAdder("chen", new AusgabeAggregator());
+        PostfixAdder cfK = new PostfixAdder("chen", new Konsolenausgabe());
         cfA.print("Hallö Welt");
         cfK.print("Hallö Welt");
     }
